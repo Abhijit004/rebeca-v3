9d7de983ba6e0e5a68b1b25e7565d8cd13737e45
Soumyajit Karmakar <sona832004@gmail.com>
2026-02-21 11:38:37 +0530

    [UDPATE]: Merge errors and bug fixes

--- client/src/screens/Home/Home.jsx
+++ client/src/screens/Home/Home.jsx
@@ -1,14 +1,15 @@
 import "./Home.css";
 import Heading from "../../components/Headingv2/Headingv2";
 import ArtistCard from "../../components/ArtistCard/ArtistCard";
-import ArtistList from "../../components/ArtistList/ArtistList"
+import ArtistList from "../../components/ArtistList/ArtistList";
 import Nightbanner from "../../components/Nightbanner/Nightbanner";
 import CollageHeart from "../../components/CollageHeart/CollageHeart";
-import Hero from "../../components/ScrollFancy/Hero"
+import Hero from "../../components/ScrollFancy/Hero";
 import CountDown from "../../components/CountDown/CountDown";
 
-const Home = () => {
+const artists = []
 
+const Home = () => {
     return (
         <div className="home">
             <div className="wave-bg">
@@ -20,25 +21,20 @@ const Home = () => {
                     <div className="">BEings, are you ready?</div>
                     <div className="">the countdown to our very own Pujo has already begun!</div>
                 </div>
-                
             </section>
             <CountDown />
             <section className="section-3">
                 <h1 className="date">MARCH 20-23</h1>
                 <h4>Lords' Ground, IIEST Shibpur</h4>
-                <p>Prepare to be swept away as you put your best foot forward in this epic celebration of creativity and culture that promises you laughter, joy and memories that will last you a lifetime and more.</p>
+                <p>
+                    Prepare to be swept away as you put your best foot forward in this epic celebration of creativity
+                    and culture that promises you laughter, joy and memories that will last you a lifetime and more.
+                </p>
             </section>
-<<<<<<< Updated upstream
-            <section className="section-4">
-                <Heading title={"ARTISTS"} />
-                {/* <Marquee images={artists.map((artist) => `/assets/imgs/artists/${artist.img}`)} /> */}
-            </section>
-=======
             <section className="section-4 ">
-    <Heading title={"ARTISTS"} />
-    <ArtistList artists={artists} />
-</section>
->>>>>>> Stashed changes
+                <Heading title={"ARTISTS"} />
+                <ArtistList artists={artists} />
+            </section>
             <section className="section-5">
                 <Heading title={"SCHEDULE"} />
             </section>
